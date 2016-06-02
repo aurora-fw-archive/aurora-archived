b257c022998a5a4528069b78c30530499b746f24
Luís Ferreira <luis_jorge_2@hotmail.com>
2016-06-02 17:28:48 +0000

    [RAM] Cross Platform integration!

--- src/Info/RAM.cpp
+++ src/Info/RAM.cpp
@@ -5,7 +5,8 @@
 #elif __linux__
 #include <sys/sysinfo.h>
 #elif __APPLE__
-#include <sys/sysctl.h>
+// #include <sys/sysctl.h>
+#include <sys/vmmeter.h>
 #endif
 
 namespace InfoRAM
@@ -16,30 +17,57 @@ namespace InfoRAM
         #ifdef __linux__
         struct sysinfo mem_temp;
         sysinfo (&mem_temp);
-        return (memInfo.totalram + memInfo.totalswap) * mem_temp.mem_unit;
+        return (mem_temp.totalram + mem_temp.totalswap) * mem_temp.mem_unit;
         #elif _WIN32
         MEMORYSTATUSEX mem_temp;
         mem_temp.dwLength = sizeof(MEMORYSTATUSEX);
         GlobalMemoryStatusEx(&mem_temp);
-        return mem_temp.ullTotalPageFile;
-        #elif __APPLE__
+        return mem_temp.ullTotalVirtual;
+        #elif defined(__MACH__) && defined(__APPLE__)
+        struct vmtotal mem_temp;
+        vmtotal (&mem_temp);
+        return mem_temp.t_vm;
         #endif
     }
     
     // Used virtual memory size in bytes
     unsigned int getUsedVirtualMemory()
     {
+        #ifdef __linux
         struct sysinfo mem_temp;
         sysinfo (&mem_temp);
         return ((memInfo.totalram - memInfo.freeram) + (memInfo.totalswap - memInfo.freeswap)) * mem_temp.mem_unit;
+        #elif _WIN32
+        MEMORYSTATUSEX mem_temp;
+        mem_temp.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&mem_temp);
+        return mem_temp.ullTotalVirtual - mem_temp.ullAvailVirtual;
+        #elif defined(__MACH__) && defined(__APPLE__)
+        struct vmtotal mem_temp;
+        struct xsw_usage swmem_temp;
+        vmtotal (&mem_temp);
+        return mem_temp.t_vm - mem_temp.t_free;
+        #endif
     }
     
     // Free virtual memory size in bytes
     unsigned int getFreeVirtualMemory()
     {
-        struct sysinfo mem_temp;
+        #ifdef __linux__
+        struct sysinfo mem_t emp;
         sysinfo (&mem_temp);
         return ((memInfo.totalram + memInfo.totalswap) - ((memInfo.totalram - memInfo.freeram) + (memInfo.totalswap - memInfo.freeswap))) * mem_temp.mem_unit;
+        #elif _WIN32
+        MEMORYSTATUSEX mem_temp;
+        mem_temp.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&mem_temp);
+        return mem_temp.ullAvailVirtual;
+        #elif defined(__MACH__) && defined(__APPLE__)
+        struct vmtotal mem_temp;
+        struct xsw_usage swmem_temp;
+        vmtotal (&mem_temp);
+        return mem_temp.t_free;
+        #endif
     }
     
     // Total pysical memory size in bytes
