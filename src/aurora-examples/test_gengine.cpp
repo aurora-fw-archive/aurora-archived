106d4a57e7c10e56a4536b8919c4f473eb9e6577
Luís Ferreira <lsferreira169@gmail.com>
2017-08-03 15:47:26 +0100

    [Examples] Update

--- src/aurora-examples/test_gengine.cpp
+++ src/aurora-examples/test_gengine.cpp
@@ -14,6 +14,16 @@ using namespace Aurora;
 Application *MyApp;
 GEngine::Application MyGApp("Test GEngine", GEngine::GraphicsAPI::OpenGL);
 
+GLfloat light_diffuse[] = {1.0f, 1.0f, 1.0f, 0.0f};  /* White diffuse light. */
+GLfloat light_position[] = {1.0f, 3.0f, 1.0f, 0.2f};  /* Infinite light location. */
+GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
+  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
+  {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
+GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
+  {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
+  {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
+GLfloat v[8][3];  /* Will be filled in with X,Y,Z vertexes. */
+
 ArSlot_t slot_Window_on_render(GEngine::Window* window) {
 	//Shell::Output << window->getWidth() << "*" << window->getHeight() << Shell::EndLine;
 	/* DEBUG ONLY!!!
@@ -23,6 +33,17 @@ ArSlot_t slot_Window_on_render(GEngine::Window* window) {
 	glVertex2f(0.5f, 0.5f);
 	glVertex2f(0.5f, -0.5f);
 	glEnd();*/
+	int i;
+
+  for (i = 0; i < 6; i++) {
+    glBegin(GL_QUADS);
+    glNormal3fv(&n[i][0]);
+    glVertex3fv(&v[faces[i][0]][0]);
+    glVertex3fv(&v[faces[i][1]][0]);
+    glVertex3fv(&v[faces[i][2]][0]);
+    glVertex3fv(&v[faces[i][3]][0]);
+    glEnd();
+  }
 	glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
 }
 
@@ -33,12 +54,43 @@ ArSlot_t slot_MyApp_on_open()
 
 
 	//DEBUG ONLY!!!
-	glClearColor(0.2f, 0.3f, 0.8f, 0.5f);
+	glClearColor(1.0f, 0.55f, 0.0f, 1.0f);
 
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 	//...
+
+	/* Setup cube vertex data. */
+  v[0][0] = v[1][0] = v[2][0] = v[3][0] = -1;
+  v[4][0] = v[5][0] = v[6][0] = v[7][0] = 1;
+  v[0][1] = v[1][1] = v[4][1] = v[5][1] = -1;
+  v[2][1] = v[3][1] = v[6][1] = v[7][1] = 1;
+  v[0][2] = v[3][2] = v[4][2] = v[7][2] = 1;
+  v[1][2] = v[2][2] = v[5][2] = v[6][2] = -1;
+
+  /* Enable a single OpenGL light. */  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHTING);
+
+  /* Use depth buffering for hidden surface elimination. */
+  glEnable(GL_DEPTH_TEST);
+
+  /* Setup the view of the cube. */
+  glMatrixMode(GL_PROJECTION);
+  gluPerspective( /* field of view in degree */ 45.0,
+    /* aspect ratio */ (4.0f/3.0f),
+    /* Z near */ 1.0, /* Z far */ 10.0);
+  glMatrixMode(GL_MODELVIEW);
+  gluLookAt(1.5f, 0.5f, 5.5f,  /* eye is at (0,0,5) */
+    0.0f, 0.0f, 0.0f,      /* center is at (0,0,0) */
+    0.0f, 1.0f, 0.0f);      /* up is in positive Y direction */
+
+  /* Adjust cube position to be asthetic angle. */
+  glTranslatef(0.0, 0.0, -1.0);
+  glRotatef(60, 1.0, 0.0, 0.0);
+  glRotatef(45, 0.0, 0.0, 1.0);
 
 	while(!window->isClosed())
 	{
--- src/aurora-examples/test_gui.cpp
+++ src/aurora-examples/test_gui.cpp
@@ -11,32 +11,34 @@
 using namespace Aurora;
 
 
-GUIApplication *MyGUIApp;
-GUIWindow *FirstWindow;
-GUILabel *HelloLabel;
+GUI::Application *MyGUIApp;
+GUI::Window *FirstWindow;
+GUI::Label *HelloLabel;
+GUI::Button *TestButton;
 
 ArSlot_t slot_MyWindow_on_open()
 {
 	HelloLabel->setText("Hello, World!");
 	HelloLabel->setSelectable(true);
 	HelloLabel->setWrap(true);
-	HelloLabel->setWrapMode(WrapMode::Word);
-	HelloLabel->setAlignment(AlignMode::Custom);
+	HelloLabel->setWrapMode(GUI::WrapMode::Word);
+	HelloLabel->setAlignment(GUI::AlignMode::Custom);
 	HelloLabel->setAlignment(0.7, 0.8);
-	//AlignMode alignLabel = HelloLabel->getAlignment();
+	GUI::AlignMode alignLabel = HelloLabel->getAlignment();
 	Shell::Log(Shell::Information,"X: ", HelloLabel->getXAlignment());
 	Shell::Log(Shell::Information,"Y: ", HelloLabel->getYAlignment());
 }
 
 ArSlot_t slot_MyGUIApp_on_open()
 {
-	FirstWindow = new GUIWindow("First Window", 800, 600, GUIWindow::NonePosition, GUIWindow::ToplevelWindow);
-	HelloLabel = new GUILabel(FirstWindow, "Hello World!");
+	FirstWindow = new GUI::Window("First Window", 800, 600, GUI::Window::NonePosition, GUI::Window::ToplevelWindow);
+	TestButton = new GUI::Button(FirstWindow, "TestButton");
+	HelloLabel = new GUI::Label(FirstWindow, "Hello World!");
 	FirstWindow->start(slot_MyWindow_on_open);
 }
 
 int main(int argc, char * argv[])
 {
-	MyGUIApp = new GUIApplication("org.aurora.example", GUIApplication::NoneFlag, slot_MyGUIApp_on_open, argc, argv);
+	MyGUIApp = new GUI::Application("org.aurora.example", GUI::Application::NoneFlag, slot_MyGUIApp_on_open, argc, argv);
 	return MyGUIApp->AppStatus;
 }
